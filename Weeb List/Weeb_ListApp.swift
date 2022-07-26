d1bf4306b41a90bc0b19f3a5a67269d57225fbd9
fullchicken44 <quanmenden@gmail.com>
2022-07-26 21:18:46 +0700

    Initial Commit

---
+++ Weeb List/Weeb_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weeb_ListApp.swift
+//  Weeb List
+//
+//  Created by Le Anh Quan on 26/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct Weeb_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
